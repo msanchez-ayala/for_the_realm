77eb83f51b8e099068f0b09b48b6e7de98d57d2b
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2023-12-05 00:30:31 -0500

    Add top right text as placeholders and style the hero name a little

--- lib/playing_card.dart
+++ lib/playing_card.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'bordered_icon.dart';
 import 'text_with_background.dart';
 
-const double cardWidth = 300;
+const double cardWidth = 310;
 const double heightWidthRatio = 1.36;
 const double cardHeight = cardWidth * heightWidthRatio;
 
@@ -50,6 +50,7 @@ class PlayingCard extends StatelessWidget {
   Widget build(BuildContext context) {
     const Color primaryRed = Color.fromARGB(255, 117, 36, 39);
     const Color primaryBlue = Color.fromARGB(255, 16, 16, 141);
+    List<String> topRightTexts = ['Aff', 'Aln'];
     List<IconWithData> stats = [
       IconWithData(text: '⚔', value: attack),
       IconWithData(text: '⛨', value: defense),
@@ -71,42 +72,46 @@ class PlayingCard extends StatelessWidget {
       child: Stack(
         children: [
           Positioned(
-              left: 0,
-              top: 0,
+              left: 20, // TODO: position dynamically instead of hacking
+              top: 7,
               child: Text(
                 heroName,
                 style: const TextStyle(
-                    fontSize: 20.0,
+                    fontSize: 22.0,
                     color: primaryRed,
                     fontWeight: FontWeight.bold),
               )),
           Positioned(
             left: 0,
             top: cardHeight / 2,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment:
-                  CrossAxisAlignment.start, // Left-align children
-              children: [
-                // Dynamically generated Text widgets
-                ...attributes.map((String item) {
-                  return TextWithBackground(
-                      text: item,
-                      backgroundColor: primaryRed,
-                      fontSize: 16.0,
-                      width: 100);
-                }).toList(),
+            child: Container(
+              // use this widget to force Column to take up `height`
+              height: cardHeight / 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment:
+                    CrossAxisAlignment.start, // Left-align children
+                children: [
+                  // Dynamically generated Text widgets
+                  ...attributes.map((String item) {
+                    return TextWithBackground(
+                        text: item,
+                        backgroundColor: primaryRed,
+                        fontSize: 14.0,
+                        width: 100);
+                  }).toList(),
 
-                // Dynamically generated Text widgets
-                ...abilities.map((String ability) {
-                  return TextWithBackground(
-                    text: ability,
-                    backgroundColor: const Color.fromARGB(255, 184, 113, 96),
-                    fontSize: 16.0,
-                    width: 230,
-                  );
-                }).toList(),
-              ],
+                  // Dynamically generated Text widgets
+                  ...abilities.map((String ability) {
+                    return TextWithBackground(
+                      text: ability,
+                      backgroundColor: const Color.fromARGB(255, 184, 113, 96),
+                      fontSize: 14.0,
+                      width: 230,
+                    );
+                  }).toList(),
+                ],
+              ),
             ),
           ),
           Positioned(
@@ -117,6 +122,19 @@ class PlayingCard extends StatelessWidget {
               crossAxisAlignment:
                   CrossAxisAlignment.end, // Right-align children
               children: [
+                Row(
+                  children: [
+                    ...topRightTexts.map((String text) {
+                      return Container(
+                          width: 65,
+                          color: primaryRed,
+                          alignment: Alignment.center,
+                          child: Text(text,
+                              style: const TextStyle(
+                                  fontSize: 30, color: Colors.white)));
+                    }).toList(),
+                  ],
+                ),
                 Text(
                   "♥" * health,
                   style: const TextStyle(fontSize: 20.0, color: primaryRed),
